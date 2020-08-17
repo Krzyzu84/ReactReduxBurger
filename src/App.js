c8468007c7d98517cedabb8dde048f8a6a14f898
unknown <krzysztofptak97@gmail.com>
2020-08-17 15:53:40 +0200

    test git branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
+import Layout from "./hoc/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
+
+//test Github Branch Authentication
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div>
         <Layout>
